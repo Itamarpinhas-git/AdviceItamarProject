91ad31daeed42ff255a766b387f9f6c6438c0f29
Itamarpinhas-git <itamar9879@gmail.com>
2025-07-29 02:04:56 +0300

    Update ElevatorCallAssignment.cs

--- Back/Models/ElevatorCallAssignment.cs
+++ Back/Models/ElevatorCallAssignment.cs
@@ -1,21 +1,20 @@
-﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ElevatorSystem.Models;
-
-public class ElevatorCallAssignment
+namespace ElevatorSystem.Models
 {
-    [Key]
-    public int CallID { get; set; } // Primary Key
-
-    public int ElevatorId { get; set; } // Foreign Key to Elevator
-
-    public int ElevatorCallId { get; set; } // Foreign Key to ElevatorCall
-
-    public DateTime AssignmentTime { get; set; }
-
-    // Navigation Properties - Simple approach
-    public virtual ElevatorCall ElevatorCall { get; set; } = null!;
-    public virtual Elevator Elevator { get; set; } = null!;
+    public class ElevatorCallAssignment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // This prevents auto-increment
+        public int CallID { get; set; }
+        
+        public DateTime AssignmentTime { get; set; }
+        public int ElevatorCallId { get; set; }
+        public int ElevatorId { get; set; }
+        
+        // Navigation properties
+        public ElevatorCall ElevatorCall { get; set; }
+        public Elevator Elevator { get; set; }
+    }
 }
